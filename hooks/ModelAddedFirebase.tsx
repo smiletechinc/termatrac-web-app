704c4140a2e808e46c897525c4aed5501617fe90
Farhan-96 <farhan96fareed@gmail.com>
2022-09-23 16:31:39 +0500

    data upload and data fetched is done

--- hooks/ModelAddedFirebase.tsx
+++ hooks/ModelAddedFirebase.tsx
@@ -1,85 +1,51 @@
 import { useCallback, useState } from "react";
-import {
-  getDatabase,
-  ref,
-  get,
-  startAt,
-  query,
-  orderByChild,
-  equalTo,
-  set,
-  child,
-  endAt,
-  push,
-} from "firebase/database";
-
+import { getDatabase, ref, get, set, push } from "firebase/database";
 import app from "../config/db";
 
-interface Props {
-  arabictext: any;
-}
-
 export const modelAddedHook = () => {
-  const [isVerseObject, setIsVerseObject] = useState(false);
-  const [verseObject, setVerseObject] = useState([]);
-  const [isSurahObject, setIsSurahObject] = useState(false);
-  const [surahObject, setSurahObject] = useState([]);
-  const [isVerseTranslate, setIsVerseTransalte] = useState(false);
-  const [verseTranslateObject, setVerseTranslateObject] = useState([]);
-  const [errorStatus, setErrorStatus] = useState("");
+  const [isModelObject, setIsModelObject] = useState(false);
+  const [modelObjectData, setModelObjectData] = useState([]);
 
-  const searchAyatTranslation = async (verseKey: string) => {
-    console.log("verseKey", verseKey);
-    const branch = `/translation/English`;
+  const dataMetaUpload = useCallback(async (dataMeta: any) => {
+    const branch = `/dataObject/`;
     if (app) {
       try {
         const db = getDatabase(app);
-        const queryRef = query(
-          ref(db, branch),
-          orderByChild("ayatId"),
-          equalTo(verseKey)
-        );
-        get(queryRef)
-          .then((response: any) => {
-            if (response.val()) {
-              setVerseTranslateObject(response.val());
-              setIsVerseTransalte(true);
-            }
+
+        var fileId = push(ref(db, branch)).key;
+        var updateData = { ...dataMeta, id: fileId };
+        set(ref(db, `/dataObject/${fileId}`), updateData)
+          .then(() => {
+            console.log("data done");
           })
           .catch((error) => {
-            console.log("No data available", error);
+            console.log("Data Not Inserted", error);
           });
       } catch (error) {
         console.log("Error Finding Database");
       }
     }
-  };
-  const searchSurahName = async (surahNumber: string) => {
-    console.log("ayatId", surahNumber);
-    const branch = `/surahMeta/`;
+  }, []);
+
+  const modelObjectFetched = useCallback(async () => {
+    setIsModelObject(false);
+    const branch = `/ModelObject/`;
     if (app) {
       try {
         const db = getDatabase(app);
-        const queryRef = query(
-          ref(db, branch),
-          orderByChild("index"),
-          equalTo(surahNumber)
-        );
-        get(queryRef)
-          .then((snapshot) => {
-            if (snapshot.exists()) {
-              setSurahObject(snapshot.val());
-              setIsSurahObject(true);
-            }
+        get(ref(db, branch))
+          .then((snapShot) => {
+            setIsModelObject(true);
+            setModelObjectData(snapShot.val());
           })
           .catch((error) => {
-            console.log("No data available in surahMeta", error);
+            console.log("Data Not Found", error);
           });
       } catch (error) {
         console.log("Error Finding Database");
       }
     }
-  };
+  }, []);
 
   const modelAddedFunction = useCallback(async (modelObject: any) => {
     const branch = `/ModelObject/`;
@@ -104,7 +70,9 @@ export const modelAddedHook = () => {
 
   return {
     modelAddedFunction,
-    searchSurahName,
-    searchAyatTranslation,
+    modelObjectFetched,
+    dataMetaUpload,
+    isModelObject,
+    modelObjectData,
   };
 };
--- pages/data/index.js
+++ pages/data/index.js
@@ -1,13 +1,40 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../../styles/Home.module.css'
-import { Header } from '../../components/header/Header'
-import { Footer } from '../../components/footer/Footer'
-import { SearchView } from '../../components/SearchView/SearchView'
-import { ResultView } from '../../components/ResultView/ResultView'
-import { TextareaAutosize, Button } from '@material-ui/core';
-
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/Home.module.css";
+import { Header } from "../../components/header/Header";
+import { Footer } from "../../components/footer/Footer";
+import { SearchView } from "../../components/SearchView/SearchView";
+import { ResultView } from "../../components/ResultView/ResultView";
+import { TextareaAutosize, Button } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import { uploadFileHook } from "../../hooks/useDataUpload";
+import { modelAddedHook } from "../../hooks/ModelAddedFirebase";
+import FilePicker, { InputErrorCode } from "@mavedev/react-file-picker";
 export default function Data() {
+  const { dataUploadService, fileUrl } = uploadFileHook();
+  const { dataMetaUpload } = modelAddedHook();
+  const [fileObject, setFile] = useState({});
+  useEffect(() => {
+    if (fileUrl != "") {
+      console.log("fileUrl", fileUrl);
+      console.log("fileObject", fileObject);
+      const fileMeta = {
+        name: fileObject.name,
+        size: fileObject.size,
+        filePath: fileUrl,
+      };
+      dataMetaUpload(fileMeta);
+    }
+  }, [fileUrl]);
+  const printAllErrors = (errorCode) => {
+    if (InputErrorCode.containsExtensionError(errorCode)) {
+      console.log("File has inappropriate extension");
+    }
+    if (InputErrorCode.containsMaxSizeError(errorCode)) {
+      console.log("File size exceeded max size specified");
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -16,10 +43,26 @@ export default function Data() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main className={styles.main}>
-      Page under construction
-      </main>
+      <main className={styles.main}>Page under construction</main>
+      <div>
+        <FilePicker
+          // maxSize={1}
+          sizeUnit="MB"
+          onFilePicked={(file) => {
+            setFile(file);
+            dataUploadService(file);
+          }}
+          onSuccess={() => {
+            console.log("Success");
+          }}
+          onError={(code) => {
+            printAllErrors(code);
+          }}
+        >
+          <button type="button">Upload file</button>
+        </FilePicker>
+      </div>
       <Footer />
     </div>
-  )
+  );
 }
